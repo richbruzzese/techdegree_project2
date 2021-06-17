7b82acf57e502b35d40b6100f4e9bca523f90964
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-06-17 16:45:29 -0400

    Students added to page
    - created a function to display list of students on page
    - variables defined to limit page display to nine students
    - conditional set to only grab the objects from the correct index point
    - created HTML in a template literal to inject the items into the page

--- js/script.js
+++ js/script.js
@@ -13,12 +13,41 @@ For assistance:
 
 
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+/* showPage function will show the initial list of students based on the page number
+called by the function.
+
+Created variables to call out to the index item in the data.js file and 
+map it to an index
+injecting HTML into the page via the studenList.innerHTML variable.
 */
-
-
+function showPage (list, page){
+const indexStart =  (page * 9) - 9 
+const indexEnd = (page* 9)
+const studentList = document.querySelector('.student-list')
+studentList.innerHTML = '';
+   for(i=0; i<list.length; i++){
+      if(i >= indexStart && i<indexEnd){
+         const studentName =  list[i].name.first + ' ' + list[i].name.last
+         const avatar = list[i].picture.large
+         const date = list[i].registered.date
+         const email = list[i].email
+         let studentItem = 
+         `<li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${avatar}">
+               <h3>${studentName}</h3>
+               <span class="email">${email}</span>
+            </div>
+            <div class = "joined details">
+             <span class = "date">${date}</span>
+            </div>
+            </li> `
+         console.log(avatar)
+         studentList.insertAdjacentHTML('beforeend', studentItem)
+      }
+   }
+}
+showPage(data, 1)
 
 /*
 Create the `addPagination` function
